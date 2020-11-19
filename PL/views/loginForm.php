21ea9a587879aa59d97406e44f0af2d2e7fe9aca
Lea KOUSSAIFI <201720311@ua.edu.lb>
2020-11-19 11:47:34 +0000

    Added loginForm.php to /PL/views

---
+++ PL/views/loginForm.php
@@ -0,0 +1,39 @@
+<?php include('login.php') ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../styles/pages/registration-page.css">
+</head>
+
+<body>
+
+    <div class="registrationForm">
+        <form method="post" id="contactForm" name="contactForm" autocomplete="off" action="login.php">
+       <center style="color: red"><?php include('errors.php'); ?></center> 
+            <h2>Login</h2>
+            <div class="form-group">
+                <input type="username" class="form-control" name="username" placeholder="Username*" required>
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" name="password" placeholder="Password*" required>
+            </div>
+
+            <div class="form-group">
+                <input type="submit" id="btn" class="btn btn-success btn-lg btn-block" value="Login" name="login_user">
+            </div>
+            <center><a href="signupForm.php">Don't have an account? Register</a></center>
+        </form>
+    </div>
+
+</body>
+
+</html>
