fd489d71969a2ce388d66965b3edfbc0f572a744
David El-Ghoul <david_ghoul@hotmail.com>
2020-11-23 17:44:25 +0200

    fixed missing userManager create user modifications

--- BLL/userManager.php
+++ BLL/userManager.php
@@ -21,4 +21,40 @@ function getStudentCourses($id){
     return selectStudentCourses($id);
 }
 
+function randomizedSalt(){
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+'; 
+    $randomString = ''; 
+  
+    for ($i = 0; $i < 6; $i++) { 
+        $index = rand(0, strlen($characters) - 1); 
+        $randomString .= $characters[$index]; 
+    } 
+  
+    return $randomString; 
+}
+
+function generatedUserId(){
+    $generatedMin = idate("Y")*pow(10,5);
+    $generatedMax  = (idate("Y")+1)*pow(10,5)-1;
+    return rand($generatedMin,$generatedMax);
+
+}
+
+function signUp($firstname,$lastname,$userType,$studentFaculty,$studentStatus,$teacherRank){
+    $userId=generatedUserId();
+    $salt=randomizedSalt();
+    $email=$userId.'@ua.edu.lb';
+    $password=mb_substr(strtoupper($firstname),0,2).mb_substr(strtoupper($lastname),0,2).substr($userId,0,4);
+    $password= md5($salt.$password.$salt);
+    
+    if($userType == 'createStudent'){
+        $studentStatusId=getStudentStatusId($studentStatus);
+        $studentFacultyId=getFacultyId($studentFaculty);
+        return createStudent($userId,$firstname,$lastname,$studentFacultyId,$studentStatusId,$email,$password,$salt);
+    }
+    else if($userType == 'createTeacher'){
+        $teacherRankId = getTeacherAcademicRankId($teacherRank);
+        createTeacher($userId,$firstname,$lastname,$teacherRankId,$email,$password,$salt);
+    }
+}
 ?>
--- DAL/connection.php
+++ DAL/connection.php
@@ -5,7 +5,7 @@ $con = "";
 function openCon(){
 // Create connection
 GLOBAL $con;
-$con = mysqli_connect('localhost:3307', 'root', '', 'lead team project');
+$con = mysqli_connect('localhost:3308', 'root', '', 'lead team project');
 // Check connection
 if (!$con) {
   die("Connection failed: " . mysqli_connect_error());
